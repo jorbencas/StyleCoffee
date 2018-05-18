815849283fce512e4e95d33711ee6d267c0e6641
Jorge Beneyto Castelló <jorbencas@gmail.com>
2018-05-18 16:26:57 +0200

    Reborn branch master

--- src/Client/app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import reactDOM  from 'react-dom';
-import App from './Components/App';
-
- $(document).ready(function(){
-    reactDOM.render(<App/>, document.getElementById('body'));
- });
--- src/Server/routes/index.js
+++
@@ -1,5 +0,0 @@
-var router = require('express').Router();
-console.log(router + "Router.js");
-router.use('/api', require('./api'));
-
-module.exports = router;
